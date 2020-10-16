c8dc17b924bf2ae02d480f4fc108d1a450f39cd8
art-ste-web <art.ste.web@gmail.com>
2020-10-16 15:21:10 +0300

    add data sort

--- res/js/modules/SheduledDatesList.js
+++ res/js/modules/SheduledDatesList.js
@@ -10,14 +10,35 @@ class SheduledDatesList extends TaskListContent {
     transformData() {
         this.mainDataArr = super.getFromLocalStorage();
         console.log(this.mainDataArr);
-        // const transformedMainArr = this.mainDataArr.filter(el => {
-        //     return el.tasks.length >0;
+        const transformedMainArr = this.mainDataArr.filter(el => {
+             return el.tasks.length > 0;
             
-        // });
-        const transArr = this.mainDataArr.forEach(element => {
-            element.date.split('.');
         });
         console.log(transformedMainArr);
+        // const transArr = transformedMainArr.map(element => {
+        //     const unixDate = element.date.split('.').reverse();
+        //     return element.date.split('.').reverse();
+        // });
+
+        transformedMainArr.forEach(element => {
+           const date = element.date.split('.').reverse();
+           const convDate = `${date[0]}-${date[1]}-${date[2]}T06:00:00`;
+           const unixTimeStamp = parseInt((new Date(convDate).getTime()/1000).toFixed(0))
+           element.unixDate = Number(unixTimeStamp);
+        });
+
+        console.log(transformedMainArr);
+
+        const sortedArr = transformedMainArr.sort((a, b)=> {
+            return (a.unixDate-b.unixDate);
+        });
+        console.log(sortedArr);
+        // const date2 =new Date ('2020-10-16T00:00:00');
+        // console.log(date2.toISOString());
+        
+        // const date1 = parseInt((new Date('2020-10-16T06:00:00').getTime()/1000).toFixed(0));
+        // console.log(date1);
+        //  console.log(transformedMainArr);
         
 
     }
