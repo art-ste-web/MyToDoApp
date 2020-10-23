290a0786a0c521287b65230cc71c0622e1a8a91d
art-ste-web <art.ste.web@gmail.com>
2020-10-23 17:53:40 +0300

    create test city search

--- weather_api/search-city.js
+++ weather_api/search-city.js
@@ -1,14 +1,34 @@
 
 const inputCity = document.getElementById("search-city");
-inputCity.addEventListener("change", searchCity(inputCity.value));
+inputCity.addEventListener("keyup", ()=> {searchCity(inputCity.value)});
 
 function searchCity(key) {
+    const cont = document.querySelector(".search-results");
+    console.log(key);
     let newArray = citiesArr.filter((el)=> {
         const caseTrans = el.cyrName.toLowerCase();
-        return caseTrans.indexOf(key) !== -1;
+        if(key!='') {
+            return caseTrans.indexOf(key) !== -1;
+        }
+        
     })
+    
+    cont.innerHTML = "";
+    newArray.forEach(el => {
+        
+        const result = document.createElement('p');
+        result.textContent = el.cyrName;
+        cont.appendChild(result);
+        
+        
+    });
+    for(let i=0; i<newArray; i++) {
+        // cont.appendChild(result);
+        console.log(newArray[i]);
+        // cont.appendChild(result.innerText[foundArr[i].cyrName]);
+    }
+    // showSearchList(newArray);
     console.log(newArray)
-    showSearchList(newArray);
 }
 
 
@@ -17,7 +37,9 @@ function showSearchList(foundArr) {
     const cont = document.querySelector(".search-results");
     const result = document.createElement('p');
     for(let i=0; i<foundArr; i++) {
-        cont.appendChild(result.innerText[foundArr[i].cyrName]);
+        // cont.appendChild(result);
+        console.log("dfdf");
+        // cont.appendChild(result.innerText[foundArr[i].cyrName]);
     }
 
 }
